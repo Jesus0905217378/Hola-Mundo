e914902d3e6c70fc573b1a6f638744d59aad3cf0
JESUS ALBERTO LOPEZ ALAY <jlopeza69@miumg.edu.gt>
2022-08-05 02:41:03 -0600

    Holamund

---
+++ src/main/java/com/parcial/holamundo/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.parcial.holamundo;
+
+/**
+ *
+ * @author Jesus Alberto
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
